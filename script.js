5040b36595c1a5796b2db191be03ccf714929829
rajathdgf <117277204+rajathdgf@users.noreply.github.com>
2024-06-22 21:24:11 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('startButton').addEventListener('click', startGame);
+
+function startGame() {
+  // Redirect to the first level
+  window.location.href = 'levels/level1.html';
+}
